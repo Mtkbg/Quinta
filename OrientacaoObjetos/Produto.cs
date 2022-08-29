c111fc0881c0e063e31f6b936a7f980f49c0f926
Mtkbg <gabrielamtkb@gmail.com>
2022-08-29 11:20:25 -0300

    primeiro

---
+++ OrientacaoObjetos/Produto.cs
@@ -0,0 +1,13 @@
+namespace OrientacaoObjetos
+{
+  class Produto
+  {
+    // public Produto(string nome, double preco)
+    // {
+    //   Nome = nome;
+    //   Preco = preco;
+    // }
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+  }
+}
---
+++ OrientacaoObjetos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OrientacaoObjetos
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Usuario usuario = new Usuario();
+      usuario.setNome("Gabi");
+      Console.WriteLine($"Nome do usuário: {usuario.getNome()}");
+
+      Produto produto1 = new Produto();
+      produto1.Nome = "Água";
+      produto1.Preco = 3.5;
+      Console.WriteLine($"Nome do produto: {produto1.Nome}");
+
+      Produto produto2 = new Produto
+      {
+        Nome = "Coca",
+        Preco = 5
+      };
+      Console.WriteLine($"Nome do produto 2: {produto2.Nome}");
+    }
+  }
+}
